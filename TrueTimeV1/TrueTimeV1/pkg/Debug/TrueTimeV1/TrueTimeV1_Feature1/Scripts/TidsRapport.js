30846eaa0e8939c5296b7026a25749e998938504
Rozhna Ramak <rozhna.ramak@stebra.se>
2016-12-14 09:30:57 +0100

    User Info Code

--- TrueTimeV1/TrueTimeV1/pkg/Debug/TrueTimeV1/TrueTimeV1_Feature1/Scripts/TidsRapport.js
+++ TrueTimeV1/TrueTimeV1/pkg/Debug/TrueTimeV1/TrueTimeV1_Feature1/Scripts/TidsRapport.js
@@ -1,72 +1,108 @@
 ﻿
-    "use strict";
-    ExecuteOrDelayUntilScriptLoaded(tidApp, "sp.js");
+"use strict";
+ExecuteOrDelayUntilScriptLoaded(tidApp, "sp.js");
 
-    function tidApp() {
-        var hostweburl;
-        var appweburl;
+function tidApp() {
 
 
-        var context = SP.ClientContext.get_current();
-        var userId = context.get_web().get_currentUser().get_id();
+
+    var context = SP.ClientContext.get_current();
+    var user = context.get_web().get_currentUser();
+    console.log("user", user);
+
+
+
+
+    // var web = context.get_web();
+    context.load(user);
+    context.executeQueryAsync(Function.createDelegate(this, onSuccess),
+            Function.createDelegate(this, onFail));
+
+    function onSuccess(sender, args) {
+        alert('user title:' + user.get_title() + '\n ID:' + user.get_id());
+        var userId = user.get_id();
         console.log("ID", userId);
-
-        // Load the required SharePoint libraries
-        $(document).ready(function () {
-            hostweburl = decodeURIComponent(getQueryStringParameter("SPHostUrl"));
-            appweburl = decodeURIComponent(getQueryStringParameter("SPAppWebUrl"));
-            var scriptbase = hostweburl + "/_layouts/15/";
-            $.getScript(scriptbase + "SP.RequestExecutor.js", execCrossDomainRequest);
-
-        });
-
-        // Function to prepare and issue the request to get SharePoint data
-        function execCrossDomainRequest() {
-            var executor = new SP.RequestExecutor(appweburl);
-
-            // Deals with the issue the call against the app web.
-            executor.executeAsync({
-                url: "https://stebra.sharepoint.com/sites/SD1/_api/Web/Lists(guid'99471df6-0ae8-46c8-9fa6-7bfb3e4bfd33')/roleassignments/GetByPrincipalId()/RoleDefinitionBindings'&$select=ID'",
-                method: "GET",
-                headers: { "Accept": "application/json; odata=verbose" },
-                success: successHandler,
-                error: errorHandler
-            }
-            );
-        }
-
-        // Function to handle the success event. Prints the data to the page.
-        function successHandler(data) {
-            var jsonObject = JSON.parse(data.body);
-            var items = [];
-            var results = jsonObject.d.results;
-            items.push("<ul>");
-
-            $(results).each(function () {
-
-                items.push('<li>' + "<a>" + this.ID + "</br>" + this.RoleTypeKind + "</a>" + '</li>');
-            });
-            Console.log("data", data);
-            items.push("</ul");
-            $("#listResult").html(items.join(''))
-
-        }
-
-
-        // Function to handle the error event. Prints the error message to the page.
-        function errorHandler(data, errorCode, errorMessage) {
-            document.getElementById("listResult").innerText = "Could not complete cross-domain call: " + errorMessage;
-        }
-
-        // Function to retrieve a query string value.
-        function getQueryStringParameter(paramToRetrieve) {
-            var params =
-                document.URL.split("?")[1].split("&");
-            var strParams = "";
-            for (var i = 0; i < params.length; i = i + 1) {
-                var singleParam = params[i].split("=");
-                if (singleParam[0] == paramToRetrieve)
-                    return singleParam[1];
-            }
-        }
     }
+    function onFail(sender, args) {
+        alert('failed to get list. Error:' + args.get_message());
+    }
+
+}
+
+var hostweburl;   
+var appweburl;   
+
+$(document).ready(function () {  
+            
+    hostweburl =   
+        decodeURIComponent(   
+            getQueryStringParameter("SPHostUrl"));   
+    appweburl =   
+        decodeURIComponent(   
+            getQueryStringParameter("SPAppWebUrl"));     
+    var scriptbase = hostweburl + "/_layouts/15/";    
+
+    $.getScript(scriptbase + "SP.RequestExecutor.js", execCrossDomainRequest);  
+});  
+              
+ 
+
+function execCrossDomainRequest() {  
+ 
+    var executor = new SP.RequestExecutor(appweburl);             
+    var listName = "Tidsrapport"
+ 
+    executor.executeAsync(  
+        {  
+                  
+            url: appweburl + "/_api/SP.AppContextSite(@target)/web/lists/getbyTitle('Tidsrapport')/Items?$select=Project%2CID%2CCreated&@target='" + hostweburl + "'",
+            
+            method: "GET",  
+            headers: { "Accept": "application/json; odata=verbose" },  
+            success: successHandler,
+            error: errorHandler
+  
+        }            
+    );
+    console.log(listName);
+}
+function successHandler(data) {
+    var jsonObject = JSON.parse(data.body);
+    var items = [];
+    var results = jsonObject.d.results;
+    console.log(results);
+    items.push("<ul>");
+
+    $(results).each(function () {
+
+        items.push('<li>' +
+                        // this.Title +
+                        "<a href=\"" + hostweburl + "/Lists/Tidsrapport/DispForm.aspx?ID=" + this.ID +
+                        "\" target=\"_blank\">" + this.Project + "</a>" + "</br>" + "<p>" + this.Created +"</p>" +
+                   '</li>');
+    });
+    console.log(this.Project);
+    console.log(this.ID);
+
+    items.push("</ul");
+    $("#listResult").html(items.join(''))
+
+    function myFunction() {
+        document.getElementById("myDropdown").classList.toggle("show");
+    }
+
+}
+
+function errorHandler(data, errorCode, errorMessage) {
+    document.getElementById("listResult").innerText = "Could not complete cross-domain call: " + errorMessage;
+}
+
+function getQueryStringParameter(paramToRetrieve) {   
+    var params =   
+        document.URL.split("?")[1].split("&");     
+    for (var i = 0; i < params.length; i = i + 1) {   
+        var singleParam = params[i].split("=");   
+        if (singleParam[0] == paramToRetrieve)   
+            return singleParam[1];   
+    }   
+}
